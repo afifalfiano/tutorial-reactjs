6a7091338eaad18ed6cd24ba63bf187dfd4c529b
Afif Alfiano <afifalfiano2@gmail.com>
2021-11-07 15:25:35 +0700

    learn: use fragment

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        // <React.Fragment>
+		//     <h1>Hello World</h1>
+        // </React.Fragment>
+        <>
+        <h1>Hello World</h1>
+        </>
+    );
+}
+
+export default Header
